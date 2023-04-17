9840a8217654276f982952d2f7a472d265b04bb4
Timofei <timofejnazarov@gmail.com>
2023-04-17 14:47:07 +0400

    Correction according to comments. Function calls one time for each element of input array

--- filter_map/filter_map.py
+++ filter_map/filter_map.py
@@ -15,4 +15,10 @@ class FilterMapExercise:
         :param input_array: Исходный список.
         :return: Отфильтрованный список.
         """
-        return [func(elem)[1] for elem in input_array if func(elem)[0] is True]
+        filtered_list = []
+        for elem in input_array:
+            function_result = func(elem)
+            if function_result[0]:
+                filtered_list.append(function_result[1])
+
+        return filtered_list
